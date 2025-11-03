07d2787ab3a532779245b64280778da7cb8f331d
Andrés Navarro <janavarroozuna@gmail.com>
2025-11-03 08:07:27 -0600

    Created step definitions

---
+++ Proyecto/requerimientos/features/step_definitions/registro_redes.steps.js
@@ -0,0 +1,77 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('el usuario está en la pantalla de registro', function () {
+  return 'pending';
+});
+
+Given('el usuario ya se registró con Google', function () {
+  return 'pending';
+});
+
+Given('el usuario tiene eventos marcados como favoritos', function () {
+  return 'pending';
+});
+
+When('inicia sesión con su cuenta de Gmail', function () {
+  return 'pending';
+});
+
+When('conecta su cuenta de Facebook', function () {
+  return 'pending';
+});
+
+When('usa su cuenta de X para autenticarse', function () {
+  return 'pending';
+});
+
+When('agrega Facebook como método adicional', function () {
+  return 'pending';
+});
+
+When('se registra con Facebook y edita su nombre antes de confirmar', function () {
+  return 'pending';
+});
+
+When('se registra con Google y sube una foto distinta a la de su cuenta', function () {
+  return 'pending';
+});
+
+When('se registra con X y acepta importar foto y nombre automáticamente', function () {
+  return 'pending';
+});
+
+When('se registra con Google y desactiva la opción de sincronizar contactos', function () {
+  return 'pending';
+});
+
+Then('se crea el perfil automáticamente con los datos de Google', function () {
+  return 'pending';
+});
+
+Then('se sincroniza su foto de perfil y se completa el registro', function () {
+  return 'pending';
+});
+
+Then('se importan sus datos básicos y se crea el perfil', function () {
+  return 'pending';
+});
+
+Then('ambos métodos quedan vinculados y se sincronizan los datos', function () {
+  return 'pending';
+});
+
+Then('el perfil se crea con el nombre personalizado', function () {
+  return 'pending';
+});
+
+Then('el perfil se crea con la foto personalizada', function () {
+  return 'pending';
+});
+
+Then('el perfil se crea con la foto y nombre de X', function () {
+  return 'pending';
+});
+
+Then('el perfil se crea sin importar contactos', function () {
+  return 'pending';
+});
